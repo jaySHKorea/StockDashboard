565675eed38dc93918e2955b07af4531dce31178
jaySHKorea <seung2586@gmail.com>
2020-08-28 13:42:11 +0900

    Add json module to pom.xml

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class BoardController {
 
+	/*
 	 @RequestMapping(value = "/main")
 	 public void krx(Model model) {
 	     String html = getHtml("http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=035420");
 	     JSONObject json = XML.toJSONObject(html);
 	     Map<String, Object> resultMap = parseJsonToMap(json.toString());
 	     model.addAttribute("map", resultMap);
-	 }
+	 }*/
 }
